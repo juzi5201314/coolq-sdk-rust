c0314f4dd984475c45106ae6f597446aa453a913
juzi5201314 <1034236490@qq.com>
2020-02-16 20:50:03 +0800

    重构了事件回调，删除了listener模块。

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #![allow(unused_attributes)]
 #![allow(non_upper_case_globals)]
 #![allow(dead_code)]
@@ -9,257 +8,208 @@
 #[macro_use]
 extern crate lazy_static;
 
+use std::cell::RefCell;
 use std::convert::{TryFrom, TryInto};
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::panic::set_hook;
 
-use encoding::{DecoderTrap, EncoderTrap, Encoding};
 use encoding::all::GB18030;
+use encoding::{DecoderTrap, EncoderTrap, Encoding};
 
 use events::*;
-use listener::*;
 
-use crate::api::{Flag, get_group_member_info_v2, set_fatal};
-use crate::qqtargets::{Authority, File, Group, GroupMember, GroupRole, Message, User};
+use crate::api::{set_fatal, Convert, Flag};
+
+pub use paste;
 
 pub mod api;
-mod cqp;
 pub mod events;
-pub mod listener;
+pub mod gen_app_json;
+pub mod targets;
 
-pub mod qqtargets;
+pub mod prelude {
+    pub use crate::api::*;
+    pub use crate::events::*;
+    pub use crate::targets::message::Message;
+}
+
+pub const APIVER: usize = 9;
 
 #[macro_export]
-macro_rules! utf8 {
-    ($e:expr) => {
-            GB18030
-                .decode(CStr::from_ptr($e).to_bytes(), DecoderTrap::Ignore)
-                .unwrap()[..]
-                .to_string()
+macro_rules! init {
+    ($main: ident) => {
+        use ::std::os::raw::c_char;
+
+        #[export_name = "AppInfo"]
+        pub extern "stdcall" fn app_info() -> *const c_char {
+            $crate::api::Convert::from(format!("{},{}", $crate::APIVER, $main::app_id())).into()
+        }
+
+        #[no_mangle]
+        pub extern "stdcall" fn on_start() -> i32 {
+            $main::start_up();
+            0
+        }
+
+
+        #[no_mangle]
+        pub extern "stdcall" fn on_enable() -> i32 {
+            $main::enable();
+            0
+        }
+
+        #[no_mangle]
+        pub extern "stdcall" fn on_disable() -> i32 {
+            $main::disable();
+            0
+        }
+
+
+        #[no_mangle]
+        pub extern "stdcall" fn on_exit() -> i32 {
+            $main::exit();
+            0
+        }
+
+        $crate::gen_event!(
+            $main,
+            (PrivateMessageEvent, on_private_msg, on_private_msg_highest, on_private_msg_high, on_private_msg_medium, on_private_msg_low;
+                sub_type: i32,
+                msg_id: i32,
+                user_id: i64,
+                msg: *const c_char,
+                font: i32
+            => i32),
+            (GroupMessageEvent, on_group_msg, on_group_msg_highest, on_group_msg_high, on_group_msg_medium, on_group_msg_low;
+                sub_type: i32,
+                msg_id: i32,
+                group_id: i64,
+                user_id: i64,
+                anonymous_flag: *const c_char,
+                msg: *const c_char,
+                font: i32
+            => i32),
+            (DiscussMessageEvent, on_discuss_msg, on_discuss_msg_highest, on_discuss_msg_high, on_discuss_msg_medium, on_discuss_msg_low;
+                sub_type: i32,
+                msg_id: i32,
+                discuss_id: i64,
+                user_id: i64,
+                msg: *const c_char,
+                font: i32
+            => i32),
+            (GroupUploadEvent, on_group_upload, on_group_upload_highest, on_group_upload_high, on_group_upload_medium, on_group_upload_low;
+                sub_type: i32,
+                send_time: i32,
+                group_id: i64,
+                user_id: i64,
+                file: *const c_char
+            => i32),
+            (GroupAdminEvent, on_group_admin, on_group_admin_highest, on_group_admin_high, on_group_admin_medium, on_group_admin_low;
+                sub_type: i32,
+                send_time: i32,
+                group_id: i64,
+                user_id: i64
+            => i32),
+            (GroupMemberDecreaseEvent, on_group_member_decrease, on_group_member_decrease_highest, on_group_member_decrease_high, on_group_member_decrease_medium, on_group_member_decrease_low;
+                sub_type: i32,
+                send_time: i32,
+                group_id: i64,
+                operate_user_id: i64,
+                being_operate_user_id: i64
+            => i32),
+            (GroupMemberIncreaseEvent, on_group_member_increase, on_group_member_increase_highest, on_group_member_increase_high, on_group_member_increase_medium, on_group_member_increase_low;
+                sub_type: i32,
+                send_time: i32,
+                group_id: i64,
+                operate_user_id: i64,
+                being_operate_user_id: i64
+            => i32),
+            (GroupBanEvent, on_group_ban, on_group_ban_highest, on_group_ban_high, on_group_ban_medium, on_group_ban_low;
+                sub_type: i32,
+                send_time: i32,
+                group_id: i64,
+                operate_user_id: i64,
+                being_operate_user_id: i64,
+                time: i64
+            => i32),
+            (FriendAddEvent, on_friend_add, on_friend_add_highest, on_friend_add_high, on_friend_add_medium, on_friend_add_low;
+                sub_type: i32,
+                send_time: i32,
+                user_id: i64
+            => i32),
+            (AddFriendRequestEvent, on_add_friend_request, on_add_friend_request_highest, on_add_friend_request_high, on_add_friend_request_medium, on_add_friend_request_low;
+                sub_type: i32,
+                send_time: i32,
+                user_id: i64,
+                msg: *const c_char,
+                flag: *const c_char
+            => i32),
+            (AddGroupRequestEvent, on_add_group_request, on_add_group_request_highest, on_add_group_request_high, on_add_group_request_medium, on_add_group_request_low;
+                sub_type: i32,
+                send_time: i32,
+                group_id: i64,
+                user_id: i64,
+                msg: *const c_char,
+                flag: *const c_char
+            => i32)
+        );
     };
 }
 
+#[doc(hidden)]
 #[macro_export]
-macro_rules! register_listener {
-    ($event_type:expr, $callback:expr) => {
-        register_listener!($event_type, $callback, coolq_sdk_rust::listener::Priority::Medium);
+macro_rules! gen_event {
+    ($main: ident, $(($event: ident, $func_name: ident, $($func_priority_name: ident),*; $($arg: ident: $t: ty),* => $result_t: ty)),*) => {
+        $(
+            $crate::gen_event!($main, $func_name, $event; $($arg: $t),* => $result_t);
+            $crate::gen_event!($main, $func_name, $event; $($arg: $t),* => $result_t; _highest);
+            $crate::gen_event!($main, $func_name, $event; $($arg: $t),* => $result_t; _high);
+            $crate::gen_event!($main, $func_name, $event; $($arg: $t),* => $result_t; _medium);
+            $crate::gen_event!($main, $func_name, $event; $($arg: $t),* => $result_t; _low);
+        )*
+
+        pub trait CQP {
+            fn app_id() -> &'static str;
+            fn start_up() {}
+            fn enable() {}
+            fn disable() {}
+            fn exit() {}
+            $(
+                fn $func_name(_: &mut $crate::events::$event) -> bool {
+                    true
+                }
+                $(
+                    fn $func_priority_name(_: &mut $crate::events::$event) -> bool {
+                        true
+                    }
+                )*
+            )*
+        }
     };
-    ($event_type:expr, $callback:expr, $priority:expr) => {
-        coolq_sdk_rust::listener::register_listener($event_type, $callback, $priority);
+
+    ($main: ident, $func_name: ident, $event: ident; $($arg: ident: $t: ty),* => $result_t: ty; $priority_name: ident) => {
+        $crate::paste::item! {
+            #[no_mangle]
+            pub extern "stdcall" fn [<$func_name $priority_name>]($($arg: $t),*) -> $result_t {
+                Convert::from(!$main::[<$func_name $priority_name>](&mut $crate::events::$event::new($($arg),*))).into()
+            }
+        }
+    };
+
+    ($main: ident, $func_name: ident, $event: ident; $($arg: ident: $t: ty),* => $result_t: ty) => {
+        #[no_mangle]
+        pub extern "stdcall" fn $func_name($($arg: $t),*) -> $result_t {
+            Convert::from($main::$func_name(&mut $crate::events::$event::new($($arg),*))).into()
+        }
     };
 }
 
-/*#[export_name = "AppInfo"]
-pub unsafe extern "stdcall" fn app_info() -> *const c_char {
-    extern "Rust" {
-        fn app_info() -> (usize, String);
-    }
-    let (version, appid) = app_info();
-    crate::api::Convert::from(format!("{},{}", version, appid).as_str()).into()
-}*/
-
-/*
 #[export_name = "Initialize"]
 pub unsafe extern "stdcall" fn initialize(auth_code: i32) -> i32 {
-    extern "Rust" {
-        fn main();
-    }
-    set_hook(Box::new(|info| { set_fatal(info.to_string()); }));
+    set_hook(Box::new(|info| {
+        set_fatal(info.to_string());
+    }));
     api::init(auth_code);
-    main();
     0
-}
-*/
-
-#[no_mangle]
-pub extern "stdcall" fn on_enable() -> i32 {
-    call_event(Events::Enable, &mut EnableEvent::default())
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_start() -> i32 {
-    call_event(Events::Start, &mut StartEvent::default())
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_disable() -> i32 {
-    call_event(Events::Disable, &mut DisableEvent::default())
-}
-
-
-#[no_mangle]
-pub extern "stdcall" fn on_exit() -> i32 {
-    call_event(Events::Exit, &mut ExitEvent::default())
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_private_msg(
-    sub_type: i32,
-    msg_id: i32,
-    user_id: i64,
-    msg: *const c_char,
-    font: i32,
-) -> i32 {
-    call_event(
-        Events::PrivateMessage,
-        &mut PrivateMessageEvent {
-            canceld: false,
-            sub_type: sub_type,
-            msg_id: msg_id,
-            msg: unsafe { Message::new(utf8!(msg)) },
-            font: font,
-            user: User::new(user_id),
-        },
-    )
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_group_msg(
-    sub_type: i32,
-    msg_id: i32,
-    group_id: i64,
-    user_id: i64,
-    anonymous_flag: *const c_char,
-    msg: *const c_char,
-    font: i32,
-) -> i32 {
-    let mut user = User::new(user_id);
-    user.set_authority(match GroupMember::try_from(get_group_member_info_v2(group_id, user_id, false).unwrap()).unwrap().role {
-        GroupRole::Owner => Authority::GroupLord,
-        GroupRole::Admin => Authority::GroupAdmin,
-        GroupRole::Member => Authority::User
-    });
-    call_event(
-        Events::GroupMessage,
-        &mut GroupMessageEvent {
-            canceld: false,
-            sub_type: sub_type,
-            msg_id: msg_id,
-            anonymous_flag: unsafe { Flag::from(utf8!(anonymous_flag)) },
-            msg: unsafe { Message::new(utf8!(msg)) },
-            font: font,
-            group: Group::new(group_id),
-            user: user,
-        },
-    )
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_discuss_msg(
-    sub_type: i32,
-    msg_id: i32,
-    discuss_id: i64,
-    user_id: i64,
-    msg: *const c_char,
-    font: i32,
-) -> i32 {
-    call_event(
-        Events::DiscussMessage,
-        &mut DiscussMessageEvent {
-            canceld: false,
-            sub_type: sub_type,
-            msg_id: msg_id,
-            discuss_id: discuss_id,
-            user_id: user_id,
-            msg: unsafe { utf8!(msg) },
-            font: font,
-        },
-    )
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_group_upload(sub_type: i32, send_time: i32, group_id: i64, user_id: i64, file: *const c_char) -> i32 {
-    call_event(Events::GroupUpload, &mut GroupUploadEvent {
-        canceld: false,
-        sub_type: sub_type,
-        group_id: group_id,
-        user_id: user_id,
-        send_time: send_time,
-        file: File::decode(unsafe { utf8!(file).as_bytes().to_vec() }),
-    })
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_group_admin(sub_type: i32, send_time: i32, group_id: i64, user_id: i64) -> i32 {
-    call_event(Events::GroupAdmin, &mut GroupAdminEvent {
-        canceld: false,
-        sub_type: sub_type,
-        send_time: send_time,
-        group: Group::new(group_id),
-        user: User::new(user_id),
-    })
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_group_member_decrease(sub_type: i32, send_time: i32, group_id: i64, operate_user_id: i64, being_operate_user_id: i64) -> i32 {
-    let being_operate_user = User::new(being_operate_user_id);
-    call_event(Events::GroupMemberDecrease, &mut GroupMemberDecreaseEvent {
-        canceld: false,
-        sub_type: sub_type,
-        group: Group::new(group_id),
-        operate_user: if sub_type == 1 { being_operate_user.clone() } else { User::new(operate_user_id) },
-        send_time: send_time,
-        being_operate_user: being_operate_user,
-    })
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_group_member_increase(sub_type: i32, send_time: i32, group_id: i64, operate_user_id: i64, being_operate_user_id: i64) -> i32 {
-    call_event(Events::GroupMemberIncrease, &mut GroupMemberIncreaseEvent {
-        canceld: false,
-        sub_type: sub_type,
-        group: Group::new(group_id),
-        operate_user: User::new(operate_user_id),
-        send_time: send_time,
-        being_operate_user: User::new(being_operate_user_id),
-    })
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_group_ban(sub_type: i32, send_time: i32, group_id: i64, operate_user_id: i64, being_operate_user_id: i64, time: i64) -> i32 {
-    call_event(Events::GroupBan, &mut GroupBanEvent {
-        canceld: false,
-        sub_type: sub_type,
-        operate_user: User::new(operate_user_id),
-        send_time: send_time,
-        being_operate_user: User::new(being_operate_user_id),
-        time: time,
-        group: Group::new(group_id),
-    })
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_friend_add(sub_type: i32, send_time: i32, user_id: i64) -> i32 {
-    call_event(Events::FriendAdd, &mut FriendAddEvent {
-        canceld: false,
-        sub_type: sub_type,
-        send_time: send_time,
-        user_id: user_id,
-    })
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_add_friend_request(sub_type: i32, send_time: i32, user_id: i64, msg: *const c_char, flag: *const c_char) -> i32 {
-    call_event(Events::AddFriendRequest, &mut AddFriendRequestEvent {
-        canceld: false,
-        sub_type: sub_type,
-        send_time: send_time,
-        msg: unsafe { utf8!(msg) },
-        flag: Flag::from(unsafe { utf8!(flag) }),
-        user: User::new(user_id),
-    })
-}
-
-#[no_mangle]
-pub extern "stdcall" fn on_add_group_request(sub_type: i32, send_time: i32, group_id: i64, user_id: i64, msg: *const c_char, flag: *const c_char) -> i32 {
-    call_event(Events::AddGroupRequest, &mut AddGroupRequestEvent {
-        canceld: false,
-        sub_type: sub_type,
-        send_time: send_time,
-        msg: unsafe { utf8!(msg) },
-        flag: Flag::from(unsafe { utf8!(flag) }),
-        group: Group::new(group_id),
-        user: User::new(user_id),
-    })
 }
