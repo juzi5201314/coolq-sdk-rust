b82477b61d91e90f98cee1f86a36e9d93e604ebc
juzi5201314 <1034236490@qq.com>
2020-02-16 20:44:01 +0800

    删除不需要的文件

--- src/cqp.rs
+++
@@ -1,8 +0,0 @@
-
-pub static EVENT_IGNORE: i32 = 0; //事件_忽略
-pub static EVENT_BLOCK: i32 = 1; //事件_拦截
-
-/*pub static REQUEST_ALLOW: i32 = 1; //请求_通过
-pub static REQUEST_DENY: i32 = 2; //请求_拒绝
-pub static REQUEST_GROUPADD: i32 = 1; //请求_群添加
-pub static REQUEST_GROUPINVITE: i32 = 2; //请求_群邀请*/
